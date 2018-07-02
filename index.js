1ea1c1e0d2d2be05de0102bd3d4846e705622e99
Roman Lidovskyi <rommxx@gmail.com>
2018-07-02 19:11:36 +0300

    Updated module with response stream handling

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+'use strict';
 const http = require('http');
 
+/**
+ * apiMultiFetcher express middleware
+ * @param req
+ * @param res
+ * @param next
+ * @returns {Promise}
+ */
 async function apiMultiFetcher(req, res, next) {
     const { query:queryParams, headers } = req;
     const [ hostname, port ] = headers.host.split(':');
@@ -14,7 +22,34 @@ async function apiMultiFetcher(req, res, next) {
     }
 
     function requestAsync (resource, requestOpts) {
-        
+        return new Promise((resolve, reject) => {
+            res.write(`"${resource}":`);
+
+            const handleWritableStream = (response) => {
+                response.on('data', (chunk) => {
+                    res.write(chunk);
+                });
+                response.on('error', (err) => {
+                    reject(err);
+                });
+                response.on('end', () => {
+                    if (resource !== resources[resources.length - 1]) {
+                        res.write(',');
+                    } else {
+                        res.write('}');
+                        res.end();
+                    }
+                    resolve();
+                });
+            };
+
+            http.get(requestOpts, (response) => {
+                handleWritableStream(response);
+            });
+
+        }).catch((err) => {
+            throw new Error(err);
+        });
     }
 
     next();
